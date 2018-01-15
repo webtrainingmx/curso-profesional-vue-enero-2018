aade5b1300f8c975121f873c697e3c5284ed37ee
Alex Arriaga <alex.arriaga.m@gmail.com>
2018-01-15 00:04:21 -0500

    Agregar ejemplo de uso de componente de cards

---
+++ country-vue/src/components/countries/CountriesHome.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="b-countries-finder">
+    <h1 class="b-title">Country List</h1>
+    <app-countries-cards :countries="countries"></app-countries-cards>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+import CountriesCards from './CountriesCards'
+
+export default {
+  name: 'app-countries-home',
+  components: {
+    'app-countries-cards': CountriesCards
+  },
+  data () {
+    return {
+      countries: null
+    }
+  },
+  methods: {
+    getCountries: function () {
+      let url = 'http://127.0.0.1:8000/countries'
+      let serviceURL = `${url}`
+
+      axios.get(serviceURL).then((response) => {
+        this.countries = response.data
+        console.log('Countries loaded!')
+      }, (error) => {
+        console.log(error)
+      })
+    }
+  },
+  mounted () {
+    this.getCountries()
+  }
+
+}
+</script>
+<style>
+.b-title {
+  text-align: center;
+  margin: 60px 0 50px 0;
+}
+
+.b-countries {
+  margin: 0 0 40px 0;
+}
+
+.b-country__flag svg {
+  width: 200px;
+  height: auto;
+}
+
+/* Element UI */
+.el-card {
+  margin-bottom: 20px;
+}
+
+.el-card__body {
+  padding: 10px 20px;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.button {
+  padding: 0;
+  float: right;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both
+}
+</style>
--- country-vue/src/router/index.js
+++ country-vue/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 import CountriesFinder from '@/components/countries/CountriesFinder'
+import CountriesHome from '@/components/countries/CountriesHome'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'CountriesFinder',
       component: CountriesFinder
+    },
+    {
+      path: '/countries',
+      name: 'CountriesHome',
+      component: CountriesHome
     }
   ]
 })
